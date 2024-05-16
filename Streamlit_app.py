01ab5b01f3f102b6311a8e65867e7eb84dd3c3c9
Tanmay Nath <meet10may@gmail.com>
2024-05-15 21:04:22 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -20,7 +20,7 @@ with st.sidebar:
 def predict(df):
    
     # model = load_pickle('Random_Forest_Classifier_day_3_with_imputation.pkl')
-    pkl_path = Path(__file__).parents[1] / 'blob/main/'
+    pkl_path = Path(__file__).parents[1] / 'mortality-predictor'
     dir_list = os.listdir(pkl_path)
     
     st.write(dir_list)
